2a8e661bcd59b021fca44a07fa6c0a5822801266
Georgi Dzhevelekov <j.djevelekov@abv.bg>
2022-07-21 18:57:10 +0300

    The entire SoftUni C# Basics course

---
+++ C# Basics/Day 2 - Exercises/T01. USD to BGN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T01._USD_to_BGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.WriteLine("Hello SoftUni!");
+            Console.Write("Hello Sunday!");
+                
+            //Console.Write only gives us a single line, while Console WriteLine lets us go one line down
+        }
+    }
+}
